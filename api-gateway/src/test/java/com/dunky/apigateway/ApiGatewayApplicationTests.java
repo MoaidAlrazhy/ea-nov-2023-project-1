20a8f6ea65715d8169f0ebd675aabf4bc706efca
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-19 09:08:47 -0600

    First microservice project folders

---
+++ api-gateway/src/test/java/com/dunky/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunky.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
